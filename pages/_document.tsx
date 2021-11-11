ab3fd6a5862680a1ff1cf087bfe5ea38ee6b9503
Michał Kurleto <mkurleto@qualtrics.com>
2021-11-11 14:59:40 +0100

    Update icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { IconContext } from 'react-icons'
+import { isDev } from 'lib/config'
+
+const basePath = isDev ? '' : '/me'
 
 export default class MyDocument extends Document {
   render() {
@@ -8,33 +11,33 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
+            <link rel='shortcut icon' href={`${basePath}/favicon.png`} />
 
             <link
               rel='apple-touch-icon'
               sizes='180x180'
-              href='/apple-touch-icon.png'
+              href={`${basePath}/apple-touch-icon.png`}
             />
             <link
               rel='icon'
               type='image/png'
               sizes='96x96'
-              href='/favicon-96x96.png'
+              href={`${basePath}/favicon-96x96.png`}
             />
             <link
               rel='icon'
               type='image/png'
               sizes='32x32'
-              href='/favicon-32x32.png'
+              href={`${basePath}/favicon-32x32.png`}
             />
             <link
               rel='icon'
               type='image/png'
               sizes='16x16'
-              href='/favicon-16x16.png'
+              href={`${basePath}/favicon-16x16.png`}
             />
 
-            <link rel='manifest' href='/manifest.json' />
+            <link rel='manifest' href={`${basePath}/manifest.json`} />
           </Head>
 
           <body>
